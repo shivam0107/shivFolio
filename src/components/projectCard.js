3742ea35fa20ca9a47973440f220b4ca959195e4
shivam singh <shivamsingh01072003@gmail.com>
2024-03-27 01:06:16 +0530

    scroll fix

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,6 +5,7 @@ import Skills from "./common/Skills";
 import Button from "./common/Button";
 
 function projectCard({
+  index,
   title,
   desc,
   pointsHeading,
@@ -17,9 +18,12 @@ function projectCard({
 }) {
   return (
     <div
-      className={` flex ${position}  flex-col gap-5  space-y-3  bg-bluegray-700 p-5 rounded-lg justify-center items-center`}
+      className={`  flex ${position} ${position === "md:flex-row" ? "md:ml-[200px]": "md:mr-[200px]" } relative flex-col mt-10 space-y-3 gap-10  bg-bluegray-700 p-5 rounded-lg justify-center items-center`}
     >
-      <div className="w-[100%] text-bluegray-50 md:w-[50%]  flex flex-col space-y-2  items-center md:items-start">
+
+    <div className={ position === "md:flex-row" ? "absolute left-0 top-[-2%]  md:top-[-6%] " : " absolute right-0 top-[-2%]  md:top-[-6%]"}><p className="text-4xl md:text-6xl font-bold bg-gradient-to-tr from-purple-500 to-yellow-100 text-transparent bg-clip-text">0{index}</p></div>
+      <div className={` w-[100%] text-bluegray-50 md:w-[50%]  flex flex-col space-y-2  items-center md:items-start`}>
+      
         <Title title={title} />
         <p>{date}</p>
         <p>{desc}</p>
